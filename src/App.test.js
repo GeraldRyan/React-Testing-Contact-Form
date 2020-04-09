c06507f9c1c72d53318a6e0b9d268f94f9bc9d78
Webbuildermn <gerald.ryan40@yahoo.com>
2020-04-09 16:48:08 -0500

    Getting the tests started

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "./components/ContactForm"
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("renders contact form without crashing", () =>{
+
+  //Arrange
+  const { getByText } = render(<ContactForm />)
+  //Act
+  const firstName = getByText(/First Name/i)
+  //Assert
+  expect(firstName).toBeInTheDocument()
+})
